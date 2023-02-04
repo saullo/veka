82592e9349f793b6d1a1422f00fb5ebb3633f0c4
Saullo Bretas Silva <saullo.silva55@outlook.com>
2023-02-03 21:21:30 -0300

    Added login page

---
+++ src/app/login/page.tsx
@@ -0,0 +1,82 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+import { useState, FormEvent } from "react";
+import { useAccount } from "@/lib/account-context";
+
+export default function Page() {
+  const { login } = useAccount();
+  const router = useRouter();
+
+  const [email, setEmail] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+  const [loading, setLoading] = useState<boolean>(false);
+
+  const handleSubmit = async (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    setLoading(true);
+
+    const request = await login(email, password);
+    if (!request) {
+      setLoading(false);
+      return;
+    }
+
+    router.push("/timeline");
+  };
+
+  return (
+    <div className="flex min-h-full items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
+      <div className="w-full max-w-md space-y-8">
+        <div>
+          <form onSubmit={handleSubmit} className="flex flex-col space-y-4">
+            <div>
+              <label
+                htmlFor="email"
+                className="block text-sm font-medium text-gray-700"
+              >
+                Email
+              </label>
+              <input
+                type="email"
+                name="email"
+                id="email"
+                disabled={loading}
+                onChange={(event) => setEmail(event.target.value)}
+                className="mt-1 block w-full rounded-md border-gray-300 shadow-sm disabled:opacity-50 focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
+              />
+            </div>
+
+            <div>
+              <label
+                htmlFor="password"
+                className="block text-sm font-medium text-gray-700"
+              >
+                Password
+              </label>
+              <input
+                type="password"
+                name="password"
+                id="password"
+                disabled={loading}
+                onChange={(event) => setPassword(event.target.value)}
+                className="mt-1 block w-full rounded-md border-gray-300 shadow-sm disabled:opacity-50 focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
+              />
+            </div>
+
+            <div>
+              <button
+                type="submit"
+                disabled={loading}
+                className="w-full rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white disabled:opacity-50 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+              >
+                Login
+              </button>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+}
