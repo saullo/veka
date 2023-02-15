2848c74ec0da38aa47eabc81b92a7c1f84f2bb9d
Saullo Bretas Silva <saullo.bretas55@gmail.com>
2023-02-14 22:30:41 -0300

    Create token with identitytoolkit

--- src/pages/api/token.ts
+++ src/pages/api/token.ts
@@ -1,10 +1,39 @@
 import admin from "@/lib/firebase";
 import { NextApiRequest, NextApiResponse } from "next";
 
+const createAuthToken = async (email: string, password: string) => {
+  const data = { email, password };
+  const url = `https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${process.env.FIREBASE_API_KEY}`;
+  const request = await fetch(url, {
+    mode: "same-origin",
+    credentials: "same-origin",
+    cache: "default",
+    method: "POST",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data),
+  });
+
+  return request.json();
+};
+
 const create = async (req: NextApiRequest, res: NextApiResponse) => {
   const { email, password } = req.body;
 
-  return res.status(200).json({});
+  const token = await createAuthToken(email, password);
+  if (token.error) {
+    const error = token.error;
+    return res.status(error.code).json({ message: error.message });
+  }
+
+  const user = await admin.auth().getUser(token.localId);
+
+  return res.status(200).json({
+    user: { id: user.uid, name: user.displayName, email: user.email },
+    token: { access_token: token.idToken },
+  });
 };
 
 const destroy = (req: NextApiRequest, res: NextApiResponse) => {};
