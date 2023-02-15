426438b9aa1a3f247b4990e84632e66b4785b8a4
Saullo Bretas Silva <saullo.bretas55@gmail.com>
2023-02-15 10:24:56 -0300

    Added blur effect on login and register

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -3,6 +3,7 @@
 import { useRouter } from "next/navigation";
 import { useState, FormEvent } from "react";
 import { useAccount } from "@/lib/account-context";
+import Link from "next/link";
 
 export default function Page() {
   const { login } = useAccount();
@@ -27,54 +28,115 @@ export default function Page() {
   };
 
   return (
-    <div className="flex min-h-full items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
-      <div className="w-full max-w-md space-y-8">
-        <div>
-          <form onSubmit={handleSubmit} className="flex flex-col space-y-4">
-            <div>
-              <label
-                htmlFor="email"
-                className="block text-sm font-medium text-gray-700"
-              >
-                Email
-              </label>
-              <input
-                type="email"
-                name="email"
-                id="email"
-                disabled={loading}
-                onChange={(event) => setEmail(event.target.value)}
-                className="mt-1 block w-full rounded-md border-gray-300 shadow-sm disabled:opacity-50 focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
+    <div className="h-full">
+      <div className="absolute inset-x-0 top-[-10rem] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[-20rem]">
+        <svg
+          className="relative left-[calc(50%-11rem)] -z-10 h-[21.1875rem] max-w-none -translate-x-1/2 rotate-[30deg] sm:left-[calc(50%-30rem)] sm:h-[42.375rem]"
+          viewBox="0 0 1155 678"
+        >
+          <path
+            fill="url(#45de2b6b-92d5-4d68-a6a0-9b9b2abad533)"
+            fillOpacity=".3"
+            d="M317.219 518.975L203.852 678 0 438.341l317.219 80.634 204.172-286.402c1.307 132.337 45.083 346.658 209.733 145.248C936.936 126.058 882.053-94.234 1031.02 41.331c119.18 108.451 130.68 295.337 121.53 375.223L855 299l21.173 362.054-558.954-142.079z"
+          />
+          <defs>
+            <linearGradient
+              id="45de2b6b-92d5-4d68-a6a0-9b9b2abad533"
+              x1="1155.49"
+              x2="-78.208"
+              y1=".177"
+              y2="474.645"
+              gradientUnits="userSpaceOnUse"
+            >
+              <stop stopColor="#9089FC" />
+              <stop offset={1} stopColor="#FF80B5" />
+            </linearGradient>
+          </defs>
+        </svg>
+      </div>
+      <div className="px-6 pt-6 lg:px-8">
+        <nav className="flex items-center justify-between" aria-label="Global">
+          <div className="flex lg:flex-1">
+            <Link href="/" className="-m-1.5 p-1.5">
+              <span className="sr-only">Your Company</span>
+              <img
+                className="h-8"
+                src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
+                alt=""
               />
+            </Link>
+          </div>
+          <div className="hidden lg:flex lg:flex-1 lg:justify-end">
+            <Link
+              href="/register"
+              className="text-sm font-semibold leading-6 text-gray-900"
+            >
+              Register <span aria-hidden="true">&rarr;</span>
+            </Link>
+          </div>
+        </nav>
+      </div>
+      <div className="flex min-h-full items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
+        <div className="w-full max-w-md space-y-8">
+          <div>
+            <div>
+              <h3 className="text-lg font-bold leading-6 text-gray-900">
+                Sign in to your account
+              </h3>
+              <p className="mt-1 max-w-2xl text-sm text-gray-700">
+                using your credentials.
+              </p>
             </div>
 
-            <div>
-              <label
-                htmlFor="password"
-                className="block text-sm font-medium text-gray-700"
-              >
-                Password
-              </label>
-              <input
-                type="password"
-                name="password"
-                id="password"
-                disabled={loading}
-                onChange={(event) => setPassword(event.target.value)}
-                className="mt-1 block w-full rounded-md border-gray-300 shadow-sm disabled:opacity-50 focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
-              />
-            </div>
+            <form
+              onSubmit={handleSubmit}
+              className="flex flex-col space-y-4 mt-4"
+            >
+              <div>
+                <label
+                  htmlFor="email"
+                  className="block text-sm font-medium text-gray-700"
+                >
+                  Email
+                </label>
+                <input
+                  type="email"
+                  name="email"
+                  id="email"
+                  disabled={loading}
+                  onChange={(event) => setEmail(event.target.value)}
+                  className="mt-1 block w-full rounded-md border-gray-300 shadow-sm disabled:opacity-50 focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
+                />
+              </div>
 
-            <div>
-              <button
-                type="submit"
-                disabled={loading}
-                className="w-full rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white disabled:opacity-50 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
-              >
-                Login
-              </button>
-            </div>
-          </form>
+              <div>
+                <label
+                  htmlFor="password"
+                  className="block text-sm font-medium text-gray-700"
+                >
+                  Password
+                </label>
+                <input
+                  type="password"
+                  name="password"
+                  id="password"
+                  disabled={loading}
+                  onChange={(event) => setPassword(event.target.value)}
+                  className="mt-1 block w-full rounded-md border-gray-300 shadow-sm disabled:opacity-50 focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
+                />
+              </div>
+
+              <div>
+                <button
+                  type="submit"
+                  disabled={loading}
+                  className="w-full rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white disabled:opacity-50 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+                >
+                  Login
+                </button>
+              </div>
+            </form>
+          </div>
         </div>
       </div>
     </div>
