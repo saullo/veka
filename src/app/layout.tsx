c6271999b13db7cfea4b9293281a8ae9020f4104
Saullo Bretas Silva <saullo.bretas55@gmail.com>
2023-02-15 01:58:45 -0300

    Added 404 not found page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,13 +8,13 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en" className="h-full">
+    <html lang="en">
       {/*
         <head /> will contain the components returned by the nearest parent
         head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
       */}
       <head />
-      <body className="h-full">
+      <body>
         <AccountProvider>{children}</AccountProvider>
         <AnalyticsWrapper />
       </body>
